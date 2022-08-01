7787e987fbf30a593eca383e26ef54fd00c0dd15
Jiaxin Pei <pedropei@vip.qq.com>
2022-08-01 09:07:55 -0700

    Merge new features developed by Jiaxin (#9)
    * automatic task assignment
    
    * simple survey page
    
    * support automatic task assignment, survey flow, login with single prolific id
    
    * trial data and surveyflow questions
    
    * allow deployer to disable jumping-to-id function
    
    * set the label text clickable
    
    * allow displaying keybindings on the main page, setting up jumping logic
    
    * set up likert intimacy
    
    * update likert data
    
    * requiring input for likert
    
    * displaying likert score under the button
    
    * fixed several bugs
    
    * task setup
    
    * update data
    
    * supporting test questions
    
    * supporting select box
    
    * update some survey flow questions
    
    * support multilingual
    
    * also updating task name for multilingual setting
    
    * fix the file conflicts when running multiple projects at the same time
    
    * bug fixed
    
    * update_survey_flow
    
    * bug fix
    
    * set up bad-text label
    
    * fix progress bar
    
    * for testing questions, set replacement for sampling
    
    * add uncommon languages
    
    * fix compatibility issue with old config files
    
    * now you can wrap text inputs as a dict and potato will automatically unfold it
    
    * allow user to config the instruction of free responses
    
    * upldate intimacy task
    
    * update instructions
    
    * update instructions
    
    * support displaying instances in columns
    
    * small issues fixed
    
    * supporting prestudy test, revise the pipeline to setup multitask annotaiton setting
    
    * displaying prestudy test result
    
    * bug fixed
    
    * small update
    
    * supporting displaying key_values for radio buttons
    
    * when automatic assignment is on, only assign real annotation instances after user consent or prestudy tests
    
    * when all the instances have been assigned, direct new users to an error page
    
    * allow displaying dynamic error messages
    
    * allow displaying dynamic error messages
    
    * fix the bug for instance sampling. Previously some instances are rarely sampled. Now we prioritize instances with less assignments
    
    * set another threshold for checking whether all the instances have been assigned
    
    * cleaning

---
+++ potato/setup_multilingual_config.py
@@ -0,0 +1,105 @@
+from argparse import ArgumentParser
+import yaml
+import os
+import pandas as pd
+import json
+from collections import defaultdict
+
+def arguments():
+    '''
+    Creates and returns the arg parser for Potato on the command line
+    '''
+    parser = ArgumentParser()
+    parser.set_defaults(show_path=False, show_similarity=False)
+
+    parser.add_argument("multilingual_config_file")
+
+    return parser.parse_args()
+
+
+
+
+def main():
+    args = arguments()
+
+    #load multilingual annotation config
+    with open(args.multilingual_config_file, 'rt') as f:
+        multilingual_config = yaml.safe_load(f)
+
+
+    #create basic folders
+    for folder in ["surveyflow", "annotation_output", "data_files", "configs", "htmls"]:
+      cur_path = multilingual_config["base_dir"] + folder
+      if not os.path.exists(cur_path):
+          os.makedirs(cur_path)
+          print("Created directory: %s" % (cur_path))
+
+
+    #build mapping dict
+    key2text = defaultdict(dict)
+
+
+    if "multilingual_guideline_file" in multilingual_config and os.path.exists(multilingual_config["multilingual_guideline_file"]):
+        #load multilingual annotation guideline
+        multilingual_guideline_df = pd.read_csv(multilingual_config["multilingual_guideline_file"])
+        for i, row in multilingual_guideline_df.iterrows():
+            if type(row['key']) != str or row['key'][0] != '[' or row['key'][-1] != ']':
+                continue
+            for lang in multilingual_config["languages"]:
+                key2text[row['key']][lang] = row[lang] if type(row[lang]) == str else row[multilingual_config["base_language"]]
+
+
+    for lang in multilingual_config["languages"]:
+        # load basic config file
+        with open(multilingual_config["base_config_file"], 'rt') as f:
+            page = f.read()
+            for key in key2text:
+                page = page.replace(key, key2text[key][lang])
+
+            surveyflow_output_path = multilingual_config["surveyflow_output_path"].replace("[LANGUAGE]", lang)
+            page = page.replace(multilingual_config["surveyflow_path"], surveyflow_output_path + lang + '-')
+            page = page.replace('[LANGUAGE]', lang)
+
+            config = yaml.safe_load(page)
+
+        config["output_annotation_dir"] = multilingual_config['output_annotation_dir'].replace("[LANGUAGE]", lang)
+        if not os.path.exists(config["output_annotation_dir"]):
+            os.makedirs(config["output_annotation_dir"])
+
+        '''
+        # setup the site_dir path for each language
+        config['path_under_site_dir'] = multilingual_config['path_under_site_dir'].replace("[LANGUAGE]", lang)
+        config["site_dir"] += config['path_under_site_dir']
+        if not os.path.exists(config["site_dir"]):
+            os.makedirs(config["site_dir"])
+        '''
+
+        config["annotation_task_name"] = multilingual_config['annotation_task_name'].replace("[LANGUAGE]", lang)
+        config["data_files"] = [it.replace("[LANGUAGE]", lang) for it in multilingual_config['data_files']]
+
+        with open(multilingual_config["base_dir"] + "configs/%s.yaml"%lang, 'wt') as f:
+            json.dump(config, f, indent=4)
+
+        # create the dir for surveyflow_output_path
+        if not os.path.exists(surveyflow_output_path):
+            os.makedirs(surveyflow_output_path)
+
+        #setup surveyflow pages
+        surveyflow_files = os.listdir(multilingual_config["surveyflow_path"])
+        for file in surveyflow_files:
+            if os.path.isdir(multilingual_config["surveyflow_path"] + file):
+                continue
+            with open(multilingual_config["surveyflow_path"] + file, 'r') as f:
+                page = f.read()
+            for key in key2text:
+                page = page.replace(key, key2text[key][lang])
+            page = page.replace('[LANGUAGE]', lang)
+            #print(surveyflow_output_path)
+            with open(surveyflow_output_path + lang + '-' +file, 'wt') as f:
+                f.write(page)
+
+        #for key in ["surveyflow_output_path", ]
+
+
+if __name__ == "__main__":
+    main()
