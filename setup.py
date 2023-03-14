31af6ef3b224da503e0184fb0273a22b9e12a3d4
Jiaxin-Pei <pedropei@vip.qq.com>
2023-03-14 16:03:07 -0400

    update MANIFEST.in and setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='potato-annotation',
-    version='1.2.0.18',
+    version='1.2.0.19',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
