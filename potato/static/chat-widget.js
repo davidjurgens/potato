eb512ff42293684fed9b540546775c5c9d29e691
Xingyao Wang <xingyao6@illinois.edu>
2023-10-31 21:28:40 -0500

    change appearance of chat bubble; disable submit button when waiting for reply

--- potato/static/chat-widget.js
+++ potato/static/chat-widget.js
@@ -9,10 +9,29 @@ class ChatWidget {
     // Custom styles for chat widget
     const style = document.createElement('style');
     style.innerHTML = `
+    .chat-message-user {
+      display: inline-block;
+      padding: 10px 15px;
+      border-radius: 18px;
+      background-color: #a6d8ff;  // Darker shade
+      color: white;
+      max-width: 80%;
+      box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
+    }
+    
+    .chat-message-reply {
+      display: inline-block;
+      padding: 10px 15px;
+      border-radius: 18px;
+      background-color: #ecf0f1;  // Lighter shade
+      color: #2c3e50;  // Dark text for light background
+      max-width: 80%;
+      box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
+    }
     `;
     document.head.appendChild(style);
   }
-  
+
   createChatWidget() {
     const annotationElement = document.querySelector('.annotation_schema');
     if (!annotationElement) return; // Ensure the element exists
@@ -24,15 +43,15 @@ class ChatWidget {
     // Modify the class of the found col-md-12 element to col-md-8
     colMd12Element.classList.remove('col-md-12');
     colMd12Element.classList.add('col-md-8');
-    colMd12Element.classList.add('ml-n3');
+    colMd12Element.classList.add('justify-content-end');
 
     // Create chat window as col-md-4
     const chatColumn = document.createElement('div');
     chatColumn.classList.add('col-md-4');
-    chatColumn.classList.add('ml-n3');
+    chatColumn.classList.add('justify-content-start');
 
     chatColumn.innerHTML = `
-    <div id="chat-widget" class="shadow d-flex flex-column" style="height: 100%; max-width: 400px; max-height: 80vh;">
+    <div id="chat-widget" class="card d-flex flex-column" style="height: 100%; max-width: 400px; max-height: 80vh;">
       <div class="card-header d-flex justify-content-between align-items-center">
           <span>Chat with Large Language Models</span>
       </div>
@@ -41,7 +60,7 @@ class ChatWidget {
       <form class="form-inline p-3">
           <div class="d-flex w-100">
               <input id="chat-input" type="text" class="form-control flex-grow-1 mr-2" placeholder="Type your message...">
-              <button id="chat-submit" type="submit" class="btn btn-primary">Send</button>
+              <button id="chat-submit" type="submit" class="btn btn-secondary">Send</button>
           </div>
       </form>
   
@@ -56,9 +75,14 @@ class ChatWidget {
 
 
   attachEventListeners() {
-    this.container.querySelector('#chat-submit').addEventListener('click', (event) => {
+
+    this.submitButton = this.container.querySelector('#chat-submit'); // Store the button in a class property for easy access
+    this.submitButton.addEventListener('click', (event) => {
         event.preventDefault();
         const message = this.container.querySelector('#chat-input').value.trim();
+        // Clear the input
+        this.container.querySelector('#chat-input').value = '';
+
         if (!message) return;
         this.onUserRequest(message);
     });
@@ -67,9 +91,13 @@ class ChatWidget {
   onUserRequest(message) {
       const messageElement = document.createElement('div');
       messageElement.className = 'text-right mb-2';
-      messageElement.innerHTML = `<span class="badge badge-dark">${message}</span>`;
+      messageElement.innerHTML = `<span class="chat-message-user">${message}</span>`;
       this.container.querySelector('#chat-messages').appendChild(messageElement);
 
+      // Disable the Send button
+      this.submitButton.disabled = true;
+
+      // TODO: replace this with API call
       setTimeout(() => {
           this.reply('Hello! This is a sample reply.');
       }, 1000);
@@ -78,8 +106,11 @@ class ChatWidget {
   reply(message) {
       const replyElement = document.createElement('div');
       replyElement.className = 'text-left mb-2';
-      replyElement.innerHTML = `<span class="badge badge-light">${message}</span>`;
+      replyElement.innerHTML = `<span class="chat-message-reply">${message}</span>`;
       this.container.querySelector('#chat-messages').appendChild(replyElement);
+
+      // Re-enable the Send button
+      this.submitButton.disabled = false;
   }
 }
 
